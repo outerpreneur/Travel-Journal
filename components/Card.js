fa65c42bb64358bfbc252b10a61ded5f3f5581a5
Alek Tysklind <91225228+outerpreneur@users.noreply.github.com>
2024-04-05 21:33:29 +0200

    google maps destination hrefs

--- components/Card.js
+++ components/Card.js
@@ -1,24 +1,32 @@
 import React from "react";
 
 export default function Card(props) {
-    console.log(props)
-    return(
-            <div className="destination-card">
-                <div className="destination-card--column-1">
-                    <img src={`${props.imageUrl}`} width="300" className="destination-card--image"/>
-                </div>
-                <div className="destination-card--column-2">
-                    <div className="destination-card--location">
-                        <img src="../images/location.png"/>
-                        <p>{props.location}</p>
-                        <a href={`${props.googleMapsUrl}`}>View in Google Maps</a>
-                    </div>
-                    <div className="destination-card--title">
-                        <h2>{props.title}</h2>
-                    </div>
-                    <h3><span>{props.startDate}</span> - <span>{props.endDate}</span></h3>
-                    <p className="destination-description">{props.description}</p>
-                </div>
-            </div>
-    )
+  console.log(props);
+  return (
+    <div className="destination-card">
+      <div className="destination-card--column-1">
+        <img
+          src={`${props.imageUrl}`}
+          width="300"
+          className="destination-card--image"
+        />
+      </div>
+      <div className="destination-card--column-2">
+        <div className="destination-card--location">
+          <img src="../images/location.png" />
+          <p>{props.location}</p>
+          <a target="_blank" href={`${props.googleMapsUrl}`}>
+            View in Google Maps
+          </a>
+        </div>
+        <div className="destination-card--title">
+          <h2>{props.title}</h2>
+        </div>
+        <h3>
+          <span>{props.startDate}</span> - <span>{props.endDate}</span>
+        </h3>
+        <p className="destination-description">{props.description}</p>
+      </div>
+    </div>
+  );
 }
--- index.pack.js
+++ index.pack.js
@@ -497,7 +497,7 @@ if (process.env.NODE_ENV === 'production') {
 
 
 Object.defineProperty(exports, "__esModule", {
-    value: true
+  value: true
 });
 exports.default = Card;
 
@@ -508,64 +508,68 @@ var _react2 = _interopRequireDefault(_react);
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 function Card(props) {
-    console.log(props);
-    return _react2.default.createElement(
+  console.log(props);
+  return _react2.default.createElement(
+    "div",
+    { className: "destination-card" },
+    _react2.default.createElement(
+      "div",
+      { className: "destination-card--column-1" },
+      _react2.default.createElement("img", {
+        src: "" + props.imageUrl,
+        width: "300",
+        className: "destination-card--image"
+      })
+    ),
+    _react2.default.createElement(
+      "div",
+      { className: "destination-card--column-2" },
+      _react2.default.createElement(
         "div",
-        { className: "destination-card" },
+        { className: "destination-card--location" },
+        _react2.default.createElement("img", { src: "../images/location.png" }),
         _react2.default.createElement(
-            "div",
-            { className: "destination-card--column-1" },
-            _react2.default.createElement("img", { src: "" + props.imageUrl, width: "300", className: "destination-card--image" })
+          "p",
+          null,
+          props.location
         ),
         _react2.default.createElement(
-            "div",
-            { className: "destination-card--column-2" },
-            _react2.default.createElement(
-                "div",
-                { className: "destination-card--location" },
-                _react2.default.createElement("img", { src: "../images/location.png" }),
-                _react2.default.createElement(
-                    "p",
-                    null,
-                    props.location
-                ),
-                _react2.default.createElement(
-                    "a",
-                    { href: "" + props.googleMapsUrl },
-                    "View in Google Maps"
-                )
-            ),
-            _react2.default.createElement(
-                "div",
-                { className: "destination-card--title" },
-                _react2.default.createElement(
-                    "h2",
-                    null,
-                    props.title
-                )
-            ),
-            _react2.default.createElement(
-                "h3",
-                null,
-                _react2.default.createElement(
-                    "span",
-                    null,
-                    props.startDate
-                ),
-                " - ",
-                _react2.default.createElement(
-                    "span",
-                    null,
-                    props.endDate
-                )
-            ),
-            _react2.default.createElement(
-                "p",
-                { className: "destination-description" },
-                props.description
-            )
+          "a",
+          { target: "_blank", href: "" + props.googleMapsUrl },
+          "View in Google Maps"
         )
-    );
+      ),
+      _react2.default.createElement(
+        "div",
+        { className: "destination-card--title" },
+        _react2.default.createElement(
+          "h2",
+          null,
+          props.title
+        )
+      ),
+      _react2.default.createElement(
+        "h3",
+        null,
+        _react2.default.createElement(
+          "span",
+          null,
+          props.startDate
+        ),
+        " - ",
+        _react2.default.createElement(
+          "span",
+          null,
+          props.endDate
+        )
+      ),
+      _react2.default.createElement(
+        "p",
+        { className: "destination-description" },
+        props.description
+      )
+    )
+  );
 }
 
 /***/ }),
@@ -607,62 +611,62 @@ function Header() {
 
 
 Object.defineProperty(exports, "__esModule", {
-    value: true
+  value: true
 });
 exports.default = [{
-    id: 1,
-    title: "Sitges & Palamos",
-    location: "Spain",
-    googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
-    startDate: "26 Jul, 2022",
-    endDate: "30 Jul, 2022",
-    description: "Catalonia boasts a blend of breathtaking landscapes, from sun-drenched Mediterranean beaches to Pyrenean peaks, alongside vibrant Barcelona and rich medieval history.",
-    imageUrl: "https://images.unsplash.com/photo-1551882998-2617b479c92f?q=80&w=3456&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+  id: 1,
+  title: "Sitges & Palamos",
+  location: "Spain",
+  googleMapsUrl: "https://maps.app.goo.gl/qpM69drZkSaxWoe29",
+  startDate: "26 Jul, 2022",
+  endDate: "30 Jul, 2022",
+  description: "Catalonia boasts a blend of breathtaking landscapes, from sun-drenched Mediterranean beaches to Pyrenean peaks, alongside vibrant Barcelona and rich medieval history.",
+  imageUrl: "https://images.unsplash.com/photo-1551882998-2617b479c92f?q=80&w=3456&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
 }, {
-    id: 2,
-    title: "Porto",
-    location: "Portugal",
-    googleMapsUrl: "https://maps.app.goo.gl/CniULpATpFUN1CCK8",
-    startDate: "13 Oct, 2022",
-    endDate: "18 Oct, 2022",
-    description: "Porto, Portugal's charming coastal city, is famed for its historic bridges, port wine cellars, and baroque architecture, showcasing a rich cultural heritage by the Douro River.",
-    imageUrl: "https://images.unsplash.com/photo-1555881400-74d7acaacd8b?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+  id: 2,
+  title: "Porto",
+  location: "Portugal",
+  googleMapsUrl: "https://maps.app.goo.gl/CniULpATpFUN1CCK8",
+  startDate: "13 Oct, 2022",
+  endDate: "18 Oct, 2022",
+  description: "Porto, Portugal's charming coastal city, is famed for its historic bridges, port wine cellars, and baroque architecture, showcasing a rich cultural heritage by the Douro River.",
+  imageUrl: "https://images.unsplash.com/photo-1555881400-74d7acaacd8b?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
 }, {
-    id: 3,
-    title: "Soller",
-    location: "Mallorca",
-    googleMapsUrl: "https://maps.app.goo.gl/bugeGWh8TiQ9cRB8A",
-    startDate: "27 Jul, 2023",
-    endDate: "30 Jul, 2023",
-    description: "Sóller, nestled in Mallorca's mountainous terrain, offers scenic tram rides, lush orange groves, quaint plazas, and easy access to the serene Port de Sóller.",
-    imageUrl: "https://images.unsplash.com/photo-1566021988761-550ac4bd5c60?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+  id: 3,
+  title: "Soller",
+  location: "Mallorca",
+  googleMapsUrl: "https://maps.app.goo.gl/bugeGWh8TiQ9cRB8A",
+  startDate: "27 Jul, 2023",
+  endDate: "30 Jul, 2023",
+  description: "Sóller, nestled in Mallorca's mountainous terrain, offers scenic tram rides, lush orange groves, quaint plazas, and easy access to the serene Port de Sóller.",
+  imageUrl: "https://images.unsplash.com/photo-1566021988761-550ac4bd5c60?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
 }, {
-    id: 4,
-    title: "Madrid",
-    location: "Spain",
-    googleMapsUrl: "https://maps.app.goo.gl/pi2NgcA2MDZgr8JK7",
-    startDate: "27 Dec, 2023",
-    endDate: "6 Jan, 2024",
-    description: "Madrid, Spain's lively capital, offers world-class museums, beautiful parks, historic squares, grand architecture, and a pulsating nightlife, encapsulating an authentic Spanish experience.",
-    imageUrl: "https://images.unsplash.com/photo-1539037116277-4db20889f2d4?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+  id: 4,
+  title: "Madrid",
+  location: "Spain",
+  googleMapsUrl: "https://maps.app.goo.gl/yQvFsNSRN6EGM3Az7",
+  startDate: "27 Dec, 2023",
+  endDate: "6 Jan, 2024",
+  description: "Madrid, Spain's lively capital, offers world-class museums, beautiful parks, historic squares, grand architecture, and a pulsating nightlife, encapsulating an authentic Spanish experience.",
+  imageUrl: "https://images.unsplash.com/photo-1539037116277-4db20889f2d4?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
 }, {
-    id: 5,
-    title: "Marrakech",
-    location: "Morocco",
-    googleMapsUrl: "https://maps.app.goo.gl/pi2NgcA2MDZgr8JK7",
-    startDate: "24 Feb, 2024",
-    endDate: "28 Feb, 2024",
-    description: "Marrakech, a vibrant Moroccan city, enchants with bustling souks, fragrant spices, majestic palaces, serene gardens, and the lively Djemaa el-Fna square's eclectic performances.",
-    imageUrl: "https://images.unsplash.com/photo-1597212618440-806262de4f6b?q=80&w=3546&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+  id: 5,
+  title: "Marrakech",
+  location: "Morocco",
+  googleMapsUrl: "https://maps.app.goo.gl/pi2NgcA2MDZgr8JK7",
+  startDate: "24 Feb, 2024",
+  endDate: "28 Feb, 2024",
+  description: "Marrakech, a vibrant Moroccan city, enchants with bustling souks, fragrant spices, majestic palaces, serene gardens, and the lively Djemaa el-Fna square's eclectic performances.",
+  imageUrl: "https://images.unsplash.com/photo-1597212618440-806262de4f6b?q=80&w=3546&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
 }, {
-    id: 6,
-    title: "Narbonne",
-    location: "France",
-    googleMapsUrl: "https://maps.app.goo.gl/pi2NgcA2MDZgr8JK7",
-    startDate: "30 Mar, 2024",
-    endDate: "7 Apr, 2024",
-    description: "Narbonne, a gem in southern France, features a rich Roman heritage, serene canals, a majestic cathedral, bustling markets, and access to nearby Mediterranean beaches.",
-    imageUrl: "https://www.campinglapinede.fr/wp-content/uploads/2023/06/narbonne-1.jpg"
+  id: 6,
+  title: "Narbonne",
+  location: "France",
+  googleMapsUrl: "https://maps.app.goo.gl/23tzJjDjMkQYvyVU7",
+  startDate: "30 Mar, 2024",
+  endDate: "7 Apr, 2024",
+  description: "Narbonne, a gem in southern France, features a rich Roman heritage, serene canals, a majestic cathedral, bustling markets, and access to nearby Mediterranean beaches.",
+  imageUrl: "https://www.campinglapinede.fr/wp-content/uploads/2023/06/narbonne-1.jpg"
 }];
 
 /***/ }),
